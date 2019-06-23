fab9e67520c8045f2df4a86c68662b36f0b3720f
Christophe <christophe.lombart@gmail.com>
2019-06-23 21:08:11 +0200

    remove tf/idf, remove underscore, review code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-
 const diacritics = require('./lib/diacritics.js');
 
 const WORD_SEPARATOR = ' ';
 const STATEMENT_SEPARATOR = '##';
-const EMPTY = '';
 
 /**
   * getStatements - Get all statements from a text
@@ -30,6 +28,10 @@ function getStatements(text) {
  * @returns {string}     the same text without specials caracters
  */
 function removeSpecials(text) {
+  if (!text) {
+    return '';
+  }
+
   const cleanText = text.replace(/[\t]/g, WORD_SEPARATOR) // Remove Tabs
                       .replace(/[\n\r]/g, WORD_SEPARATOR)
                       .replace(/&nbsp;/gi, WORD_SEPARATOR)// remove HTML entities, only non breaking space
@@ -50,7 +52,21 @@ function removeSpecials(text) {
   // remove multiple spaces
   result = result.replace(/\s+/g, WORD_SEPARATOR);
 
-  return result;
+  return result.trim();
+}
+
+/**
+ * removeLineBreaks - Remove line breaks in a text
+ *
+ * @param  {type} text the text
+ * @returns {type}     the text without line breaks
+ */
+function removeLineBreaks(text) {
+  if (!text) {
+    return '';
+  }
+
+  return text.replace(/(\r\n|\n|\r)/gm, '').trim();
 }
 
 /**
@@ -93,14 +109,14 @@ function getWords(text, withStopWords, language) {
 
   // Remove empty string
   if (withStopWords) {
-    return _.filter(words, (word) => word !== '');
+    return words.filter((word) => word !== '');
   }
 
   // Remove empty string & stopwords
 
   const { stopwords } = require(`./lib/stopwords-${ language }`);
 
-  return _.filter(words, (word) => word !== '' && stopwords.indexOf(removeDiacritics(word)) === -1);
+  return words.filter((word) => word !== '' && stopwords.indexOf(removeDiacritics(word)) === -1);
 }
 
 /**
@@ -121,173 +137,16 @@ function getWords(text, withStopWords, language) {
 function getNgrams(words, n) {
   const result = [];
 
-  const count = _.max([ 0, words.length - n + 1 ]);
+  const count = Math.max(0, words.length - n + 1);
 
   for (let i = 0; i < count; i++) {
     const slice = words.slice(i, i + n);
 
     // Convert the ngram array into a ngram string and add it in the result list
-    result.push(_.reduce(slice, (memo, word) => memo ? `${ memo } ${ word }` : word));
+    result.push(slice.reduce((memo, word) => memo ? `${ memo } ${ word }` : word));
   }
 
   return result;
-}
-
-/**
- * getTf - Get the term frequency (tf) of each word of a document
- *
- * @param  {string} words array of words (String) matching to the document content
- * @param  {number} n     cardinality of the ngrams (optional). Has to be > 0
- * @param  {object} stats the current stat aout the word or ngrams (could be null). This is used to compute the tf/idf
- * @returns {object}      F json structure :
- *    {
- *        count : an array of word with the number of occurence in the document (the array index matches to the word)
- *        tf    : an array of tf value of each word (the array index matches to the word)
- *        max   : the value of the most frequent word
- *    }
- */
-function getTf(words, n, stats) {
-  let ws = words;
-
-  if (n && n > 1) {
-    ws = getNgrams(words, n);
-  }
-
-  const count = _.countBy(ws, (word) => word);
-  const max = _.max(count);
-  const tfs = {};
-
-  _.keys(count).forEach((word) => {
-    // Calculate the tf for this word
-    tfs[word] = count[word] / max;
-
-    // Update stats
-    if (stats) {
-      // Calculate sum & register the tf for min & max computation
-      if (stats.has(word) && stats.get(word).tfs) {
-        const wordStat = stats.get(word);
-
-        // update the number of documents for this word
-        wordStat.nbrDocs++;
-
-        // Add the tf in the list of all tfs for this word
-        wordStat.tfs.push(tfs[word]);
-        wordStat.tfSum += tfs[word];
-      } else {
-        const newWordStat = initWordStat(word, tfs[word]);
-
-        stats.set(word, newWordStat);
-      }
-    }
-  });
-
-  return {
-    count,
-    tfs,
-    max
-  };
-}
-
-/**
-  * geTfIdf - Get the tfIdf for each word of a document
-  *
-  * @param  {object} document the document represented by an term frequency array.
-  * @param  {number} nbrDocs  the number of documents
-  * @param  {object} stats    stats about the words for the full set of documents
-  * @returns {object}         the tf/idf info for this document
-  */
-function geTfIdf(document, nbrDocs, stats) {
-  const tfIdf = {};
-
-  _.keys(document.tfs).forEach((word) => {
-    const idf = Math.log(nbrDocs / stats.get(word).nbrDocs) + 1;
-
-    tfIdf[word] = document.tfs[word] * idf;
-
-    if (stats.has(word) && stats.get(word).tfIdfs && stats.get(word).idfs) {
-      const wordStat = stats.get(word);
-
-      wordStat.tfIdfs.push(tfIdf[word]);
-      wordStat.tfIdfSum += tfIdf[word];
-      wordStat.idfs.push(idf);
-      wordStat.idfSum += idf;
-    }
-  });
-
-  document.tfIdf = tfIdf;
-
-  return document;
-}
-
-/**
- *  Get the TF.IDF for each words found in several documents
- *
- * @param an arrays of String matching to the document content. It could be Text or HTML
- * @param ngrams cardinality (optional). Has to be > 0
- * @param True if the stop words are to be present in the corpus
- */
-
-/**
-  * getTfIdfs - Get the TF.IDF for each words found in several documents
-  *
-  * @param  {Array} documents    arrays of String matching to the document content. It could be Text or HTML
-  * @param  {number} n             ngram cardinality (optional). Has to be > 0
-  * @param  {boolean} withStopWords if true, remove the stopwords
-  * @param  {string} language      the language code (fr, en, ... )
-  * @returns {object}              the tf/idf for each word/ngrams
-  */
-function getTfIdfs(documents, n, withStopWords, language) {
-  const result = {};
-  const stats = new Map();
-
-  // Calculate the TF of each words for each docs
-  const tfs = _.map(documents, (document) => getTf(getWords(document, withStopWords, language), n, stats));
-
-  // Calculate the tf.idf for each each docs & produce stats per word
-  const data = _.map(tfs, (docTfs) => geTfIdf(docTfs, documents.length, stats));
-
-  // Calculate stats : min, max, avg for tf, idf & tf.idf
-  for (const wordStat of stats.values()) {
-    wordStat.tfMin = _.min(wordStat.tfs);
-    wordStat.tfMax = _.max(wordStat.tfs);
-    wordStat.tfAvg = wordStat.tfSum / wordStat.nbrDocs;
-
-    wordStat.idfMax = _.max(wordStat.idfs);
-    wordStat.idfAvg = wordStat.idfSum / wordStat.nbrDocs;
-
-    wordStat.tfIdfMin = _.min(wordStat.tfIdfs);
-    wordStat.tfIdfMax = _.max(wordStat.tfIdfs);
-    wordStat.tfIdfAvg = wordStat.tfIdfSum / wordStat.nbrDocs;
-  }
-
-  result.documents = data;
-  result.numberOfDocs = documents.length;
-  result.stats = stats;
-
-  return result;
-}
-
-/**
- * initWordStat - Create an new Stat object for one word
- *
- * @param  {string} word the word
- * @param  {number} tf   the tf value
- * @returns {object}     the stat about the word
- */
-function initWordStat(word, tf) {
-  return {
-    word,
-    nbrDocs: 1,
-    tfSum: tf,
-    tfs: [ tf ],
-
-    idfSum: 0,
-    idfs: [],
-
-    tfIdfSum: 0,
-    tfIdfs: []
-
-  };
 }
 
 exports.getStatements = getStatements;
@@ -296,10 +155,8 @@ exports.removeSpecials = removeSpecials;
 
 exports.removeDiacritics = removeDiacritics;
 
+exports.removeLineBreaks = removeLineBreaks;
+
 exports.getWords = getWords;
 
 exports.getNgrams = getNgrams;
-
-exports.getTf = getTf;
-
-exports.getTfIdfs = getTfIdfs;
--- test/test.js
+++ test/test.js
@@ -1,123 +1,89 @@
-var assert     = require("assert");
-var _          = require("underscore");
-var natural    = require("../index.js");
+const assert = require('assert');
+const natural = require('../index.js');
 
+describe('Natural Content', () => {
+  const documents = [
+    'word1 word2 word3 word4 word5 word6. word7 word1 word8 word9 word10 word11 word6. word1 word12 word13. word1 word1 ',
+    'word2 word7 word8 word9 word10 word7 word11 word7 word11 word11 word11 word11.',
+    ' word7 word2 '
+  ];
 
-describe("Natural Content", function() {
-        var documents = [
-            "word1 word2 word3 word4 word5 word6. word7 word1 word8 word9 word10 word11 word6. word1 word12 word13. word1 word1 ",
-            "word2 word7 word8 word9 word10 word7 word11 word7 word11 word11 word11 word11.",
-            " word7 word2 "  ];
+  const documentsFr = [
+    'Les conditions d\'utilisations de l\'objet doivent se faire dans de bonnes conditions. Sinon l\'objet ne peut pas bien être utilisé.',
+    'Les conditions d\'emploi de la chose doivent se faire dans de bonne condition. Sinon l\'objet n\'est pas utilisable.',
+    'Pour éviter une mauvaise utilisation, les conditions d\'utilisations doivent être faite correctement.'
+  ];
 
-        var documentsFr = [
-          "Les conditions d'utilisations de l'objet doivent se faire dans de bonnes conditions. Sinon l'objet ne peut pas bien être utilisé.",
-          "Les conditions d'emploi de la chose doivent se faire dans de bonne condition. Sinon l'objet n'est pas utilisable.",
-          "Pour éviter une mauvaise utilisation, les conditions d'utilisations doivent être faite correctement."
-        ];
-        it('Statements', function() {
-              var stats = natural.getStatements("word1 word2 word3 word4 :word5 word6. word7 word1, word8 word9 word10 word11 word6. word1 word12 word13");
-              assert(stats.length === 3);
+  it('Statements', () => {
+    const stats = natural.getStatements('word1 word2 word3 word4 :word5 word6. word7 word1, word8 word9 word10 word11 word6. word1 word12 word13');
 
-        });
+    assert(stats.length === 3);
+  });
 
-        it('Special caracters', function(){
-            var text = "ceci est un texte en français ! sans caractères spéciaux !§($€) # 123 avant-hier";
-            var result = natural.removeSpecials(text);
-            
-            assert( result === 'ceci est un texte en français sans caractères spéciaux avant hier');
-        });
+  it('Special caracters', () => {
+    const text = 'ceci est un texte en français ! sans caractères spéciaux !§($€) # 123 avant-hier';
+    const result = natural.removeSpecials(text);
 
+    assert(result === 'ceci est un texte en français sans caractères spéciaux avant hier');
+  });
 
-        it('apostrophe', function(){
-            var text = "ceci est un texte en français. l'été sera chaud. Les conditions d'utilisation de l'objet";
+  it('apostrophe', () => {
+    const text = 'ceci est un texte en français. l\'été sera chaud. Les conditions d\'utilisation de l\'objet';
 
-            var result = natural.removeSpecials(text);
-            assert( result === 'ceci est un texte en français l été sera chaud Les conditions d utilisation de l objet');
-        });
+    const result = natural.removeSpecials(text);
 
-        it('diacritics', function(){
-            var text = "ceci est un texte en français ! sans diacritiques çàoözęùô";
-            var result = natural.removeDiacritics(text);
-            assert(result === 'ceci est un texte en francais ! sans diacritiques caoozeuo');
-        });
+    assert(result === 'ceci est un texte en français l été sera chaud Les conditions d utilisation de l objet');
+  });
 
-        it('Words', function() {
+  it('diacritics', () => {
+    const text = 'ceci est un texte en français ! sans diacritiques çàoözęùô';
+    const result = natural.removeDiacritics(text);
 
-          var words = natural.getWords("word1 word2 word3 word4. le la sur word5, ça été bientôt & @' $ 123", true);
-          assert(words.length === 15);
+    assert(result === 'ceci est un texte en francais ! sans diacritiques caoozeuo');
+  });
 
-          words = natural.getWords(natural.removeSpecials("word1 word2 word3 word4. le la sur word5, ça été bientôt & @' $ 123"), true);
-          //console.log("words", words);
-          assert(words.length === 11);
+  it('Words', () => {
+    let words = natural.getWords('word1 word2 word3 word4. le la sur word5, ça été bientôt & @\' $ 123', true);
 
-          words = natural.getWords("word1 word2 word3 word4. le la sur word5 bientôt", false, "fr");
-          //console.log("words", words);
-          assert(words.length === 5);
+    assert(words.length === 15);
 
-          words = natural.getWords("word1 word2 word3 1234 word4 156,78. le la sur word5", false, "fr");
-          //console.log(words);
-          assert(words.length === 8);
+    words = natural.getWords(natural.removeSpecials('word1 word2 word3 word4. le la sur word5, ça été bientôt & @\' $ 123'), true);
 
-          words = natural.getWords("l'été sera chaud. Les conditions d'utilisation de l'objet", false, "fr");
-          assert(words.length === 4);
+    // console.log("words", words);
+    assert(words.length === 11);
 
-          words = natural.getWords("l'été sera chaud. Les conditions d'utilisation de l'objet", true);
-          assert(words.length === 11);
-        });
+    words = natural.getWords('word1 word2 word3 word4. le la sur word5 bientôt', false, 'fr');
 
-        it('n-grams', function() {
-          var grams = natural.getNgrams(natural.getWords("word1 word2 word3 word4. le la sur word5", true), 1);
-          assert(grams.length === 8);
-          assert(grams[3] === "word4");
+    // console.log("words", words);
+    assert(words.length === 5);
 
-          grams = natural.getNgrams(natural.getWords("word1 word2 word3 word4. le la sur word5", true), 2);
-          assert(grams.length === 7);
-          assert(grams[1] === "word2 word3");
+    words = natural.getWords('word1 word2 word3 1234 word4 156,78. le la sur word5', false, 'fr');
 
-          grams = natural.getNgrams(natural.getWords("word1 word2 word3 word4. le la sur word5", true), 3);
-          assert(grams.length === 6);
-          assert(grams[2] === "word3 word4 le");
+    // console.log(words);
+    assert(words.length === 8);
 
-          grams = natural.getNgrams(natural.getWords("l'été sera chaud. Les conditions d'utilisation de l'objet", false, "fr"), 1);
-          assert(grams.length === 4);
+    words = natural.getWords('l\'été sera chaud. Les conditions d\'utilisation de l\'objet', false, 'fr');
+    assert(words.length === 4);
 
+    words = natural.getWords('l\'été sera chaud. Les conditions d\'utilisation de l\'objet', true);
+    assert(words.length === 11);
+  });
 
-        });
+  it('n-grams', () => {
+    let grams = natural.getNgrams(natural.getWords('word1 word2 word3 word4. le la sur word5', true), 1);
 
-        it("term frequency", function(){
-            var info = natural.getTf(natural.getWords(documents[0], false, "fr"));
-            //console.log(info);
-            assert(info.max === 5);
-            assert(info.count.word1 === 5);
-            assert(info.count.word6 === 2);
-            assert(info.tfs.word1 === 1);
-            assert(info.tfs.word2 === 0.2);
-            assert(info.tfs.word6 === 0.4);
+    assert(grams.length === 8);
+    assert(grams[3] === 'word4');
 
-        });
+    grams = natural.getNgrams(natural.getWords('word1 word2 word3 word4. le la sur word5', true), 2);
+    assert(grams.length === 7);
+    assert(grams[1] === 'word2 word3');
 
+    grams = natural.getNgrams(natural.getWords('word1 word2 word3 word4. le la sur word5', true), 3);
+    assert(grams.length === 6);
+    assert(grams[2] === 'word3 word4 le');
 
-        it("tf.idf for a set of document ", function() {
-
-            var info = natural.getTfIdfs(documents, 1, false, "fr");
-
-            var sorted = _.sortBy(Array.from(info.stats.values()), function(word) { return -word.tfIdfSum;});
-            assert(sorted[0].word === "word1");
-            //console.log(sorted);
-
-        });
-
-        /*
-        it.only("tf.idf for a set of document in french", function() {
-
-            var info = natural.getTfIdfs(documentsFr, 3, true);
-
-            var sorted = _.sortBy(Array.from(info.stats.values()), function(word) { return -word.tfIdfSum;});
-            //assert(sorted[0].word === "word1");
-            console.log(sorted);
-
-        });
-        */
-
-
+    grams = natural.getNgrams(natural.getWords('l\'été sera chaud. Les conditions d\'utilisation de l\'objet', false, 'fr'), 1);
+    assert(grams.length === 4);
+  });
 });
